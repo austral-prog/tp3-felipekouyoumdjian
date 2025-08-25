518f5c771c5c5e65c3a6e680b2c2319f382a4cd1
felipekouyoumdjian <fkouyoumdjian@mail.austral.edu.ar>
2025-08-25 12:49:47 -0300

    Add files via upload

--- in_string.py
+++ in_string.py
@@ -1,6 +1,13 @@
 def check_vowels():
+    nombre = input()
+    print("Contiene a:","a" in nombre or "A" in nombre)
+    print("Contiene e:","e" in nombre or "E" in nombre)
+    print("Contiene i:","i" in nombre or "I" in nombre)
+    print("Contiene o:","o" in nombre or "O" in nombre)
+    print("Contiene u:","u" in nombre or "U" in nombre)
+
     # Código a implementar utilizando input.
 
-
+#check_vowels()
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,11 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+    texto = input()
+    print(texto[4::2])
 
+
+
+
+    # Código a implementar utilizando input.
+#slice_advanced()
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
